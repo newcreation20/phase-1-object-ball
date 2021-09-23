f690f706bc0577010b0e704657acee22d0c02bfb
Kerny <kerny14@gmail.com>
2021-09-23 11:27:15 -0400

    Objects

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -6,13 +6,55 @@ function gameObject(){
       players: {
 
         "Alan Anderson": {
-          number:
-          shoe
+          number: 0,
+          shoe: 16,
+          Points: 22,
+          Rebounds: 12,
+          Assists: 12,
+          Steals:3,
+          Blocks: 1,
+          SlamDunks:1,
         },
-        "Reggie Evans": {},
-        "Brook Lopez": {},
-        "Mason Plumlee": {},
-        "Jason Terry": {}
+        "Reggie Evans": {
+          number:30,
+          shoe:14,
+          Points:12,
+          Rebounds:12,
+          Assists:12,
+          Steals:12,
+          Blocks:12,
+          SlamDunks:7
+        },
+        "Brook Lopez": {
+          number: 11,
+          shoe:17,
+          Points:17,
+          Rebounds:19,
+          Assists:10,
+          Steals:3,
+          Blocks:1,
+          SlamDunks:15
+        },
+        "Mason Plumlee": {
+          number:1,
+          shoe:19,
+          Points:26,
+          Rebounds:12,
+          Assists:6,
+          Steals:3,
+          Blocks:8,
+          SlamDunks:5
+        },
+        "Jason Terry": {
+          number:31,
+          shoe:15,
+          Points:19,
+          Rebounds:2,
+          Assists:2,
+          Steals:4,
+          Blocks:11,
+          SlamDunks:1
+        }
       }
 
     },
@@ -21,12 +63,66 @@ function gameObject(){
       colors:["turquoise", "purple"],
       players: {
         
-        "Jeff Adrien": {},
-        "Bismak Biyombo": {},
-        "DeSagna Diop": {},
-        "Ben Gordon": {},
-        "Brendan Haywood": {}
+        "Jeff Adrien": {
+          Number:4,
+          Shoe:18,
+          Points:10,
+          Rebounds:1,
+          Assists:1,
+          Steals:2,
+          Blocks:7,
+          SlamDunks:2
+        },
+        "Bismak Biyombo": {
+          Number:0,
+          Shoe:16,
+          Points:12,
+          Rebounds:4,
+          Assists:7,
+          Steals:7,
+          Blocks:15,
+          SlamDunks:10
+        },
+        "DeSagna Diop": {
+          Number:2,
+          Shoe:14,
+          Points:24,
+          Rebounds:12,
+          Assists:12,
+          Steals:4,
+          Blocks:5,
+          SlamDunks:5
+        },
+        "Ben Gordon": {
+          Number:8,
+          Shoe:15,
+          Points:33,
+          Rebounds:3,
+          Assists:2,
+          Steals:1,
+          Blocks:1,
+          SlamDunks:0
+        },
+        "Brendan Haywood": {
+          Number:33,
+          Shoe:15,
+          Points:6,
+          Rebounds:12,
+          Assists:12,
+          Steals:22,
+          Blocks:5,
+          SlamDunks:12
+        }
 
       }
     }
   }
+}
+console.log(gameObject());
+
+function numPointsScored() {
+
+  return "22"
+}
+const result = numPointsScored()
+console.log(numPointsScored);
