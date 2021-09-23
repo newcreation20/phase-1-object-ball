a40fd5db6a844408ccd57d9653ebe94359f88e9c
priscilla <pdjarbeng@outlook.com>
2021-09-23 10:26:45 -0400

    object

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -1,15 +1,32 @@
-function gameObject(home){
-    home
+function gameObject(){
+  return {
+    home: {
+      teamName:"Brooklyn Nets",
+      colors:["black","white"],
+      players: {
 
-    return 
-    // home: {
-    //   teamName:
-    //   colors:
-    //   players:
-    // }
-    // away: {
-    //   teamName:
-    //   colors:
-    //   players:
-    // }
+        "Alan Anderson": {
+          number:
+          shoe
+        },
+        "Reggie Evans": {},
+        "Brook Lopez": {},
+        "Mason Plumlee": {},
+        "Jason Terry": {}
+      }
+
+    },
+    away: {
+      teamName:"Charlotte Hornets",
+      colors:["turquoise", "purple"],
+      players: {
+        
+        "Jeff Adrien": {},
+        "Bismak Biyombo": {},
+        "DeSagna Diop": {},
+        "Ben Gordon": {},
+        "Brendan Haywood": {}
+
+      }
+    }
   }
