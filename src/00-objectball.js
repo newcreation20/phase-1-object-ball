beb0b6e6b3cdc7c58ab1871e6fab2fb8d98ae250
Kerny <kerny14@gmail.com>
2021-09-23 09:50:50 -0400

    Object

---
+++ src/00-objectball.js
@@ -0,0 +1,15 @@
+function gameObject(home){
+    home
+
+    return 
+    // home: {
+    //   teamName:
+    //   colors:
+    //   players:
+    // }
+    // away: {
+    //   teamName:
+    //   colors:
+    //   players:
+    // }
+  }
